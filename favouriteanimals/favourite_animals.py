ce85e113557f851a0bc34fad521704c444df9fa5
faklijta <faklijta@gmail.com>
2017-10-06 11:16:29 +0200

    feat(favourite animals) basic functions complete

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -7,3 +7,34 @@
 # One animal should be stored only at once
 # Each animal should be written in separate lines
 # The program should only save animals, no need to print them
+
+import sys
+
+def get_args():
+    return sys.argv
+
+
+def add_animals(animal):
+    with open("favourites.txt", "a") as file:
+        file.write(str(animal) + '\n')
+
+
+def read_file(file):
+    with open(file) as f:
+        return f.read()
+
+
+def check_if_listed(file):
+    animals_list = read_file(file).split()
+    pass
+
+
+def controller():
+    arguments = get_args()
+    if len(arguments) == 1:
+        print("Usage:\n ```fav_animals [animal] [animal]```")
+    elif len(arguments) >= 2:
+        add_animals(sys.argv[1:])
+
+
+controller()
